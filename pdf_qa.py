f93505b7c4a995d1a95c9fcc1660fd7e6c70241f
kman <qu4rt3rb4k3d@gmail.com>
2023-11-25 18:09:12 -0800

    add batch mode

--- pdf_qa.py
+++ pdf_qa.py
@@ -47,6 +47,7 @@ parser.add_argument('--mmr', action='store_true', help='use maximal marginal rel
 parser.add_argument('--no-chunking', action='store_true', help='disable chunking and vectorstore')
 parser.add_argument('--model', type=str, default='gpt-4', help='model to use for QA')
 parser.add_argument('--tokens', action='store_true', help='display the length in tokens of the input document(s)')
+parser.add_argument('--batch', type=str, default=None, help='name of batch (enables batch mode if specified)')
 args = parser.parse_args()
 
 url = args.url
@@ -58,6 +59,7 @@ mmr = args.mmr
 chunking = not args.no_chunking
 qa_model = args.model
 show_tokens = args.tokens
+batch_name = args.batch
 
 assert sum(map(lambda a: a is not None, [url, file_path, dir_path])) == 1, 'need to provide a URL, a file path, or a directory path'
 assert chunking or not dir_path, 'loading multiple documents with chunking disabled is not currently supported'
@@ -89,46 +91,55 @@ elif dir_path:
 
 if chunking:
     vectorstore = get_vectorstore(docs, chunk_length, qa_model)
-else:
-    context_string = reduce(lambda a, b: a + b, map(lambda c: c.page_content, docs))
-
-if show_tokens:
+if not chunking or show_tokens:
     doc_string = reduce(lambda a, b: a + b, map(lambda c: c.page_content, docs))
-    print(get_num_tokens(doc_string, qa_model))
+if show_tokens:
+    print(f'num_tokens = {get_num_tokens(doc_string, qa_model)}')
 
-log_file = open('log.txt', 'a')
-now = datetime.datetime.now()
-log_file.write('Conversation started at ' + now.strftime('%Y-%m-%d %H:%M:%S') + ':\n')
-
-message_log = []
-
-while True:
-    query = input('Q: ')
-    if query == 'exit':
-        break
-    log_file.write('Q: ' + query + '\n')
-
-    if chunking:
-        if mmr:
-            chunks = vectorstore.max_marginal_relevance_search(query=query, k=num_chunks, fetch_k=num_chunks*4)
-        else:
-            chunks = vectorstore.similarity_search(query=query, k=num_chunks)
-        context_string = str(reduce(lambda a, b: a + b, map(lambda c: [c.page_content], chunks)))
-        #chunks_string = str(reduce(lambda a, b: a + b, map(lambda c: str(c), chunks)))
-        system_message = 'The above are excerpts from one or more research papers, followed by previous questions and responses. Use this information to answer the following questions.'
-    else:
-        system_message = 'The above is a research paper, followed by previous questions and responses. Use this information to answer the following questions.'
-
+def get_response(context, instructions, query, log=[]):
     messages = []
-    messages.append({'role': 'user', 'content': context_string})
-    messages.extend(message_log)
-    messages.append({'role': 'system', 'content': system_message})
+    messages.append({'role': 'user', 'content': context})
+    messages.extend(log)
+    messages.append({'role': 'system', 'content': instructions})
     messages.append({'role': 'user', 'content': query})
+    return dispatch_chat_completion(messages, client, model=qa_model)
 
-    response = dispatch_chat_completion(messages, client, model=qa_model)
-    message_log.append({'role': 'user', 'content': query})
-    message_log.append({'role': 'assistant', 'content': response})
-    print('\nA: ' + response + '\n')
-    log_file.write('\nA: ' + response + '\n\n')
+if batch_name is not None:
+    with open(batch_name + '.in.txt', 'r') as file:
+        batched_queries = file.read()
+    instructions = "The above is a research paper (text extracted from a PDF) followed by some questions about it. Please summarize the paper and then answer the questions."
+    response = get_response(doc_string, instructions, batched_queries)
+    print(response)
+    with open(batch_name + '.out.txt', 'w') as file:
+        file.write(response)
+else:
+    log_file = open('log.txt', 'a')
+    now = datetime.datetime.now()
+    log_file.write('Conversation started at ' + now.strftime('%Y-%m-%d %H:%M:%S') + ':\n')
 
-log_file.close()
+    message_log = []
+
+    while True:
+        query = input('Q: ')
+        if query == 'exit':
+            break
+        log_file.write('Q: ' + query + '\n')
+
+        if chunking:
+            if mmr:
+                chunks = vectorstore.max_marginal_relevance_search(query=query, k=num_chunks, fetch_k=num_chunks*4)
+            else:
+                chunks = vectorstore.similarity_search(query=query, k=num_chunks)
+            context_string = str(reduce(lambda a, b: a + b, map(lambda c: [c.page_content], chunks)))
+            #chunks_string = str(reduce(lambda a, b: a + b, map(lambda c: str(c), chunks)))
+            system_message = 'The above are excerpts from one or more research papers, followed by previous questions and responses. Use this information to answer the following questions.'
+        else:
+            context_string = doc_string
+            system_message = 'The above is a research paper, followed by previous questions and responses. Use this information to answer the following questions.'
+
+        response = get_response(context_string, system_message, query, message_log)
+        message_log.append({'role': 'user', 'content': query})
+        message_log.append({'role': 'assistant', 'content': response})
+        print('\nA: ' + response + '\n')
+        log_file.write('\nA: ' + response + '\n\n')
+    log_file.close()
